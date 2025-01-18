3fcca5fed1b193a9869a32226d8b3a651498bc88
FuzzyGiraffee <121840200+FuzzyGiraffee@users.noreply.github.com>
2025-01-17 21:05:20 -0600

    readded tailwind to package.json

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import { Container } from '@mui/material' ;
 export const meta: MetaFunction = () => {
   return [
     { title: "Rooster Ranges Cockatrice" },
@@ -9,14 +9,12 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
+    
         <header className="flex flex-col items-center gap-9">
           <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
             Welcome To Rooster Ranges
           </h1>         
         </header>    
-      </div>
-    </div>
+
   );
 }
